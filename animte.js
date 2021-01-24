4c627a9e6d885284969500c31a26a346f986f42e
wilson azer <wilsonazer@hotmail.com>
2021-01-23 22:23:32 -0300

    add js to animation

---
+++ animte.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const h1 = document.querySelector('h1');
+    h1.style.animationPlayState = 'paused'
+    document.querySelector('button').onclick = ()=>{
+        if(h1.style.animationPlayState === 'paused' ){
+           h1.style.animationPlayState = 'running'
+        }else{
+            h1.style.animationPlayState = 'paused'
+        }
+    }
+})
